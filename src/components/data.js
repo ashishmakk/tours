cd448802ee44e04c4fe90f9f57f453f63ea7ca8e
unknown <ashishn.makwana@gmail.com>
2024-03-08 21:25:01 +0530

    first commit

---
+++ src/components/data.js
@@ -0,0 +1,91 @@
+import img1 from "../images/tour-1.jpeg"
+import img2 from "../images/tour-2.jpeg"
+import img3 from "../images/tour-3.jpeg"
+import img4 from "../images/tour-4.jpeg"
+import img5 from "../images/tour-5.jpeg"
+import img6 from "../images/rtr.jpg"
+
+export const navLinks = [
+{id: 1, text: 'home', link: '#home'},
+{id: 2, text: 'about', link: '#about'},
+{id: 3, text: 'services', link: '#services'},
+{id: 4, text: 'tours', link: '#tours'},
+{id: 4, text: 'tours', link: '#tours'},
+{id: 4, text: 'tours', link: '#tours'},
+]
+
+export const services = [
+    {   id: 1, 
+        icon: "fas fa-wallet fa-fw", 
+        title: "saving money", 
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."
+    },
+
+    {   id: 2, 
+        icon: "fas fa-wallet fa-fw", 
+        title: "endless hiking", 
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."
+    },
+
+    {   id: 3, 
+        icon: "fas fa-wallet fa-fw", 
+        title: "amazing comfort", 
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."
+    }
+]
+
+export const tours = [
+       {id: 1, 
+        image: img1, 
+        date: "august 26th, 2020", 
+        title: "Tibet Adventure", 
+        details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.", 
+        location: "china", 
+        duration: "6 days", 
+        cost: "$2100"},
+
+        {id: 2, 
+        image: img2, 
+        date: "august 26th, 2020", 
+        title: "best of java", 
+        details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.", 
+        location: "indonesia", 
+        duration: "8 days", 
+        cost: "$4100"},
+
+        {id: 3, 
+        image: img3, 
+        date: "august 26th, 2020", 
+        title: "explore hong kong", 
+        details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.", 
+        location: "hong kong", 
+        duration: "4 days", 
+        cost: "$2300"},
+
+        {id: 4, 
+        image: img4, 
+        date: "august 26th, 2020", 
+        title: "kenya highlights", 
+        details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.", 
+        location: "kenya", 
+        duration: "8 days", 
+        cost: "$2400"},
+
+        {id: 5, 
+        image: img5, 
+        date: "august 26th, 2020", 
+        title: "Paris highlights", 
+        details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.", 
+        location: "pahi", 
+        duration: "1 days", 
+        cost: "$3100"},
+
+        {id: 6, 
+        image: img6, 
+        date: "august 26th, 2020", 
+        title: "Ukraine highlights", 
+        details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.", 
+        location: "Ukraine", 
+        duration: "62 days", 
+        cost: "11100"},
+]
